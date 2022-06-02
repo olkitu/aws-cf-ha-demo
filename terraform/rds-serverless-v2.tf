69d8ee589b5e99f8296ec262af6de9b8996d1e25
Minh Truong <minh@truong.fi>
2022-06-02 19:35:58 +0300

    Add second instance for HA

--- terraform/rds-serverless-v2.tf
+++ terraform/rds-serverless-v2.tf
@@ -32,6 +32,13 @@ resource "aws_rds_cluster_instance" "rds-serverless-v2" {
   engine_version     = aws_rds_cluster.rds-serverless-v2.engine_version
 }
 
+resource "aws_rds_cluster_instance" "rds-serverless-v2-2" {
+  cluster_identifier = aws_rds_cluster.rds-serverless-v2.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.rds-serverless-v2.engine
+  engine_version     = aws_rds_cluster.rds-serverless-v2.engine_version
+}
+
 resource "aws_db_subnet_group" "rds-serverless-v2" {
   subnet_ids = [
     data.aws_cloudformation_export.cf-ha-vpc-PrivateSubnet1AID.value,
